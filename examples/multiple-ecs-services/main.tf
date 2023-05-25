689c1be789adefb68fc78eaf663dc08c8efaeeca
JerinRathnam <61724686+jerinrathnam@users.noreply.github.com>
2023-05-25 17:00:21 +0530

    Modify container port and host port

--- examples/multiple-ecs-services/main.tf
+++ examples/multiple-ecs-services/main.tf
@@ -3,9 +3,9 @@ module "ecs-cluster" {
 
   cluster_name = "my-cluster"
   name         = ["service-1", "service-2", "service-3"]
-  port         = [80] #container port
-  # port = [80, 3000, 4000] # this is for multiple container ports
-  host_port       = [8080, 8081, 8082] # if container port and host ports are same 
+  # port       = [8080] # If container port same for all services
+  port            = [8080, 3000, 4000] # If Ports are different for all services
+  host_port       = [8080, 8081, 8082] # Need only If container port and host ports are not same. If both are same This can be command out
   region          = "us-east-2"
   security_groups = ["sg-0123456789abcdef"]
   subnet_ids      = ["subnet-s123f7f546", "subnet-32g745g56"]
